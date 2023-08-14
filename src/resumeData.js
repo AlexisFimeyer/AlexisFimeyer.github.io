094395485139fc86a44e68ee7a6cbba014f4316e
AlexisFimeyer <a.h.p.a.fimeyer@student.tudelft.nl>
2023-08-14 21:02:47 +0200

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,113 @@
+let resumeData = {
+  imagebaseurl: "AlexisFimeyer.github.io/",
+  name: "Alexis Fimeyer",
+  role: "Student at TUDelft in computer science and engineering",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "github",
+      url: "https://github.com/AlexisFimeyer",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "I am currently a second year student at TUDelft and pursuing my bachelor in computer science and engineering. Currently diving deeper into Machine Learning and Big Data Processing with the data variant.",
+  address: "Netherlands",
+  website: "https://AlexisFimeyer.github.io",
+  education: [
+    {
+      UniversityName: "Delft University of Technology",
+      specialization: "Data",
+      MonthOfPassing: "July",
+      YearOfPassing: "2023",
+      Achievements: "First year completed",
+    },
+    {
+      UniversityName: "École Européenne de Bruxelles II",
+      specialization: "Mathematics, physics and chemistry",
+      MonthOfPassing: "July",
+      YearOfPassing: "2022",
+      Achievements: "European baccalaureate",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Stanford University",
+      specialization: "Machine Learning",
+      MonthOfLeaving: "2021",
+      YearOfLeaving: "",
+      Achievements: {
+        text: "Certificate of completion (97.46%)",
+        url: "https://coursera.org/share/66a2da2d7403df4926b08dc76b26d0a2",
+      },
+    },
+    {
+      CompanyName: "Michigan University",
+      specialization: "Applied Machine Learning in Python",
+      MonthOfLeaving: "2021",
+      YearOfLeaving: "",
+      Achievements: {
+        text: "Certificate of completion (92.04%)",
+        url: "https://coursera.org/share/8638ab3bf0cef84337ff2fdfc8cda3e9",
+      },
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "java",
+    },
+    {
+      skillname: "python",
+    },
+    {
+      skillname: "javascript",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Octave",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Assembly BitMap encoder",
+      description: "Script",
+      imgurl: "/images/project-2.png",
+      url:
+        "https://github.com/AlexisFimeyer/Assembly-projects/tree/main/BitMap%20Encoder",
+    },
+    {
+      name: "SHA-1 implemented in Assembly",
+      description: "Script",
+      imgurl: "/images/project-1.png",
+      url: "https://github.com/AlexisFimeyer/Assembly-projects/tree/main/SHA1",
+    },
+    {
+      name: "Talio - A java based board sharing app",
+      description: "App",
+      imgurl: "images/project-3.png",
+      url: "https://github.com/AlexisFimeyer/Talio",
+    },
+    {
+      name: "A BrainFuck interpreter written in Assembly",
+      description: "Script",
+      imgurl: "images/project-4.png",
+      url:
+        "https://github.com/AlexisFimeyer/Assembly-projects/tree/main/BrainFuck",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
